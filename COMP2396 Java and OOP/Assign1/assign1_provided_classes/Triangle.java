b337e81f1ea04fb367df1d853d28af9d6b3c46c7
ysalvin <ysalvin@connect.hku.hk>
2024-01-06 03:15:23 +0800

    feat: project after year 3 sem1

--- COMP2396 Java and OOP/Assign1/assign1_provided_classes/Triangle.java
+++
@@ -1,8 +0,0 @@
-public class Triangle extends Shape {
-    public void setVertices(double d) {
-        this.xLocal = new double[3];
-        this.yLocal = new double[3];
-        this.xLocal={d,(-d*Math.cos(Math.PI/3)),(-d*Math.cos(Math.PI/3))};
-        this.yLocal={0,(-d*Math.sin(Math.PI/3)),(d*Math.sin(Math.PI/3))};
-    }
-}
